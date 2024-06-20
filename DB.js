4f35c3cfb5ba1c288a7d238b247a1c68d23c1f12
이연승 <leeys951006@gmail.com>
2024-06-20 15:08:10 +0900

    DB 내용 수정

--- DB.js
+++ DB.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./mydatabase.db');
 
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT');
+  db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT)');
 
   db.run("INSERT INTO books (title, author) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald')");
   db.run("INSERT INTO books (title, author) VALUES ('To kill a Mockingbird', 'Harper Lee')");
@@ -11,7 +11,7 @@ db.serialize(() => {
 
   db.each('SELECT id, title, author FROM books', (err, row) => {
     if (err) {
-      console.err(err.message);
+      console.error(err.message);
       return;
     }
     console.log(`${row.id} - ${row.title} by ${row.author}`);
